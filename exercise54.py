6f97b7cf0f6d16e6353211edcd49f4f5a673f457
marklevi <mdayanlevi@gmail.com>
2013-03-21 11:45:33 -0400

    https://github.com/marklevi/dev-sprint1.git

--- exercise54.py
+++ exercise54.py
@@ -1,2 +1,21 @@
-# This is where Exercise 5.4 goes
-# Name:
+#exercise 5.4
+
+Mark Levi 
+
+
+def is_triangle(a,b,c):
+	if a > (b+c):
+		print "No"
+	elif b > (a+c):
+			print "No"
+	elif c > (a+b):
+			print "No"
+	else:
+		print "Yes"
+
+a = int(raw_input('What is the length of the first side?\n'))
+b = int(raw_input('What is the length of the second side?\n'))
+c = int(raw_input('What is the length of the third side?\n'))
+
+is_triangle(a,b,c)
+
